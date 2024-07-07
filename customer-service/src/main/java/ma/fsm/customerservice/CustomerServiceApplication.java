5df1fd7c4f214ff8ec1fb96625286232b77580d2
R-ELKADAH <rachidelkadah@email.com>
2024-07-07 21:51:25 +0100

    utilisation de config sur customer et le push sur github

--- customer-service/src/main/java/ma/fsm/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/ma/fsm/customerservice/CustomerServiceApplication.java
@@ -1,15 +1,18 @@
 package ma.fsm.customerservice;
 
+import ma.fsm.customerservice.config.GlobalConfig;
 import ma.fsm.customerservice.entities.Customer;
 import ma.fsm.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GlobalConfig.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
